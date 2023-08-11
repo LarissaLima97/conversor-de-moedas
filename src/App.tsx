a49274ebb56cad9bd18be557ae34442d50ff574e
LarissaLima97 <larissa.lmaria@gmail.com>
2023-08-11 16:30:22 -0300

    Testing Header and Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './styles/global.scss';
 import { Box } from './components/Box';
+import { Input } from './components/Input';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Box>
-        <p>OOOO</p>
-        <p>OOOO</p>
-        <p>OOOO</p>
-        <p>OOOO</p>
-        <p>OOOO</p>
+        <Header>Conversor de Moedas</Header>
+        <Input></Input>
       </Box>
     </div>
   );
