2e70e0a42ea5ccd0aeff2c7c818f3ffbee094237
LarissaLima97 <larissa.lmaria@gmail.com>
2023-12-12 18:58:08 -0300

    Creating function to catch values from the inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/global.scss';
 import { Box } from './components/Box';
 import { Input } from './components/Input';
@@ -10,18 +10,41 @@ import {convertValue} from './services/api';
 
 function App() {
   let arr: string[] = [];
-  arr = ['BRL', 'USD', 'EUR']; 
+  arr = ['','BRL', 'USD', 'EUR']; 
+
+  const [stateValue, setStateValue] = useState({
+    userValue: 0,
+    currencyFrom:'',
+    currencyTo:'',
+
+  });
+
+  const handleChange = (e:React.FormEvent<HTMLInputElement>) => {
+    const value = e.currentTarget.value;
+    setStateValue({...stateValue, 
+      [e.currentTarget.name]: value
+    });
+  }
   return (
     <div className="App">
       <Box>
         <Header>Conversor de Moedas</Header>
         <Label>
           Valor        
-          <Input></Input>
+          <Input
+            name="userValue"
+            value={stateValue.userValue}
+            onChange={handleChange}
+            
+          />
         </Label>
         <Label>
           De
-          <DropDown>
+          <DropDown
+            name='currencyFrom'
+            value={stateValue.currencyFrom}
+            onChange={handleChange}
+          >
             {arr.map(item => (
               <option>{item}</option>
             ))}
@@ -29,13 +52,17 @@ function App() {
         </Label>
         <Label>
           Para
-          <DropDown>
+          <DropDown
+            name='currencyTo'
+            value={stateValue.currencyTo}
+            onChange={handleChange}
+          >
             {arr.map(item => (
               <option>{item}</option>
             ))}
           </DropDown>
         </Label>
-        <Button onClick={()=> convertValue('USD', 'BRL',30)}>Converter</Button>
+        <Button onClick={()=> convertValue(stateValue.currencyFrom, stateValue.currencyTo,stateValue.userValue)}>Converter</Button>
       </Box>
     </div>
   );
